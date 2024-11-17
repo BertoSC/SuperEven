732dda1f0e60f7056e37d2311818c8ce6c6cca8f
BertoSC <a23albertogc@iessanclemente.net>
2024-11-17 14:36:07 +0100

    first version

--- src/main/java/org/example/CheckSuperEven.java
+++ src/main/java/org/example/CheckSuperEven.java
@@ -2,40 +2,40 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class CheckSuperEven implements Callable {
     private Number num;
-    private static final ExecutorService EX = Executors.newFixedThreadPool(2);
+    private ExecutorService pool;
 
     public CheckSuperEven (Number i){
         this.num=i;
+        this.pool= Executors.newFixedThreadPool(2);
     }
 
     @Override
     public String call() throws Exception {
         char[] number = String.valueOf(num.getNum()).toCharArray();
         for (char c : number) {
-            Future<Boolean> b = EX.submit(new CheckEven(Character.getNumericValue(c)));
+            Future<Boolean> b = pool.submit(new CheckEven(Character.getNumericValue(c)));
             boolean res = b.get();
-            if (!res) {
-                num.setSupereven(false);
-                break;
-            }
-        }
 
-        EX.shutdown();
+            if (!res) {
+                    num.setSupereven(false);
+                }
+            }
 
         String result;
+
         if (num.getSuperEven()) {
             result = num.getNum() + " is super even";
         } else {
             result = num.getNum() + " is not super even";
         }
+
+        pool.shutdown();
+
         return result;
     }
 }
